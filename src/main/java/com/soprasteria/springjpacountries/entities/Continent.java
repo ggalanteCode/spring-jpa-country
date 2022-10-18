c81f750bd2ed42d17792d94995bcd4c99da651d4
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-18 16:53:02 +0200

    relazione ManyToOne da regions a continents

--- src/main/java/com/soprasteria/springjpacountries/entities/Continent.java
+++ src/main/java/com/soprasteria/springjpacountries/entities/Continent.java
@@ -7,27 +7,21 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "regions")
-public class Region {
+@Table(name = "continents")
+public class Continent {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Integer region_id;
+	private Integer continent_id;
 	
 	@Column(nullable = false)
 	private String name;
 	
-	@OneToMany(mappedBy = "countries")
-	private List<Country> countries;
-	
-	@ManyToOne
-	@JoinColumn(nullable = false, name = "continent_id")
-	private Continent continent;
+	@OneToMany(mappedBy = "regions")
+	private List<Region> regions;
 
 }
