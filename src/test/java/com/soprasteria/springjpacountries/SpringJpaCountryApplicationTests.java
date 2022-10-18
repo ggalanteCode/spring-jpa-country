bc9c02c84c7f8de168345dee7672da1a07864af2
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-18 15:36:11 +0200

    database dei paesi con spring e jpa

---
+++ src/test/java/com/soprasteria/springjpacountries/SpringJpaCountryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soprasteria.springjpacountries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaCountryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
