1d2a84adf02fab6214addce8bfd48a8e3d43834e
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-18 15:57:45 +0200

    classe per la tabella countries del db

---
+++ src/main/java/com/soprasteria/springjpacountries/entities/Country.java
@@ -0,0 +1,30 @@
+package com.soprasteria.springjpacountries.entities;
+
+import java.math.BigDecimal;
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Country {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer country_id;
+	
+	private String name;
+	
+	@Column(nullable = false)
+	private BigDecimal area;
+	
+	private Date national_day;
+	
+	private String country_code2;
+	
+	private String country_code3;
+
+}
