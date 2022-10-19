eea4dd90069f8e45dcf3de4b93ca1a5a86b76090
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-19 15:07:05 +0200

    classi restcontroller per country, region e continent

---
+++ src/main/java/com/soprasteria/springjpacountries/controller/ContinentController.java
@@ -0,0 +1,10 @@
+package com.soprasteria.springjpacountries.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/continent")
+public class ContinentController {
+
+}
