556ed2ba0b496d914758e51397d8f8d0638cf68c
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-10-20 15:57:44 +0200

    crea (POST) un nuovo country e aggiorna (PUT) un country già esistente

--- src/main/java/com/soprasteria/springjpacountries/controller/CountryController.java
+++ src/main/java/com/soprasteria/springjpacountries/controller/CountryController.java
@@ -5,8 +5,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -50,6 +55,28 @@ public class CountryController {
 			return new ArrayList<>();
 		}
 		
+	}
+	
+	@PostMapping("/insertcountry")
+	public ResponseEntity<Country> insertNewCountry(@RequestBody Country country) {
+		return new ResponseEntity<Country>(countryRepository.save(country), HttpStatus.OK);
+	}
+	
+	@PutMapping("/updatecountry/{countryID}")
+	public ResponseEntity<Country> updateACountry(@PathVariable(value = "countryID", required = true) Integer countryID,
+												  @RequestBody Country country) {
+		Optional<Country> optional = countryRepository.findById(countryID);
+		if(optional.isPresent()) {
+			Country updatedCountry = optional.get();
+			updatedCountry.setArea(country.getArea());
+			updatedCountry.setCountry_code2(country.getCountry_code2());
+			updatedCountry.setCountry_code3(country.getCountry_code3());
+			updatedCountry.setName(country.getName());
+			updatedCountry.setRegion(country.getRegion());
+			return new ResponseEntity<Country>(countryRepository.save(updatedCountry),HttpStatus.OK);
+		} else {
+			return new ResponseEntity<Country>((Country)null,HttpStatus.NOT_FOUND);
+		}
 	}
 
 }
